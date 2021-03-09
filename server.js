53c87d830890d438756d7c4a5623d05a058c0930
Ruchi <rsimhar746@gmail.com>
2021-03-09 10:59:36 +0530

    practical commit

---
+++ server.js
@@ -0,0 +1,8 @@
+// modules =================================================
+const express = require ( 'express' );
+const app = express ();
+// set our port
+const port = 3000 ;
+app.get ( '/' , ( req , res ) => res.send ( 'Welcome to Mean Full Stack web Development! Ruchi,1805548' ));// startup our app at http://localhost:3000
+app.listen ( port , () =>console.log ( `Example app listening on port ${port}!` ));
+
